9b2df236bc6294d1c692b73fda242e4903e5b36a
axemonkey <hi+github@clivemurray.com>
2025-09-14 14:53:46 +0100

    remove manifest and service worker etc

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,7 +1,5 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/public');
-	eleventyConfig.addPassthroughCopy('src/manifest.json');
-	eleventyConfig.addPassthroughCopy('src/service-worker.js');
 	eleventyConfig.addPassthroughCopy({'src/robots.txt': '/robots.txt'});
 	eleventyConfig.setUseGitIgnore(false);
 	eleventyConfig.setServerOptions({
